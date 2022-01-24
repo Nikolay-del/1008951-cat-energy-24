058005c46002a554aa79467b020401cc0113c09f
nikolay <vdregdes@gmail.com>
2022-01-24 15:03:52 +0300

    PP index.html

--- gulpfile.js
+++ gulpfile.js
@@ -105,6 +105,8 @@ const copy = (done) => {
   gulp.src([
       'source/fonts/*.{woff2,woff}',
       'source/*.ico',
+      'source/*webmanifest',
+      'source/favicons',
     ], {
       base: 'source'
     })
